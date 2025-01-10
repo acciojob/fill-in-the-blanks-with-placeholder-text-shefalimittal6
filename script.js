b018ea20ca8f65b1dd452ca457b08074cb31f89b
Shefali <mittal.shefali2000@gmail.com>
2025-01-10 19:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-//your code here
+document.getElementById('accio-text').innerHTML = 
+    "Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size.";
